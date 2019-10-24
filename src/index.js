95c2de836cbba5f926814c314a6b167a07494493
ruxiu.zheng <18202764523@163.com>
2019-10-24 09:56:26 +0800

    redu单独使用

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import { createStore } from 'redux'
+// 1.新建store
+// 通过reducer建立
+// 根据老的state和action生成新的state
+function counter(state=0, action){
+    switch(action.type) {
+        case '加机关枪':
+            return state + 1
+        case '减机关枪':
+            return state - 1
+        default:
+            return 10
+    }
+}
+const store = createStore(counter)
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const init = store.getState()
+console.log(init)
+// 订阅事件
+function listener(){
+    const current = store.getState()
+    console.log(`现在有机关枪${current}把`)
+}
+store.subscribe(listener)
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// 派发事件，传递action
+store.dispatch({type: '加机关枪'})
+store.dispatch({type: '加机关枪'})
