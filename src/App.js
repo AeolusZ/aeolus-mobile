1dfa0d7bb6bb6344330bb91f9912c0a1d40e7b88
ruxiu.zheng <18202764523@163.com>
2019-10-25 14:26:41 +0800

    react-router使用以及用装饰器方式来写connect

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
 import React from 'react'
-// import { addGUN } from'./index.redux'
 import { Button } from 'antd-mobile'
+import { connect } from 'react-redux'
+import { addGUN, removeGUN, addGUNAsync } from './index.redux'
+const mapStateToProps= (state)=> {
+  return {num:state}
+}
+const actionCreatores = {addGUN, removeGUN, addGUNAsync}
+@connect(mapStateToProps, actionCreatores)
  class App extends React.Component{
    render(){
-     const store = this.props.store
-     const num = store.getState()
-     const addGUN = this.props.addGUN
-     const removeGUN = this.props.removeGUN
-     const addGUNAsync = this.props.addGUNAsync
+     const { num, addGUN, removeGUN, addGUNAsync } = this.props
      return (<div>
         <h1>现在有机枪 {num}  把</h1>
-        <Button onClick={()=> store.dispatch(addGUN())}>申请武器</Button>
-        <Button onClick={()=> store.dispatch(removeGUN())}>回收武器</Button>
-        <Button onClick={()=> store.dispatch(addGUNAsync())}>申请武器,拖两天再给</Button>
+        <Button onClick={addGUN}>申请武器</Button>
+        <Button onClick={removeGUN}>回收武器</Button>
+        <Button onClick={addGUNAsync}>申请武器,拖两天再给</Button>
      </div>
       )
    }
  }
+
 export default App
--- src/index.js
+++ src/index.js
@@ -2,16 +2,18 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
+import { Provider } from 'react-redux'
 
 import App from './App'
 import { counter, addGUN, removeGUN, addGUNAsync } from './index.redux'
 
 const store = createStore(counter, applyMiddleware(thunk))
-function render() {
-    console.log(store.getState())
-    ReactDOM.render(<App store={store} addGUNAsync={addGUNAsync} addGUN={addGUN} removeGUN={removeGUN}/>, document.getElementById('root'))
-}
-store.subscribe(render)
-render()
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    document.getElementById('root'))
+
 
 
